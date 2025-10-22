46ff130efc723cff4bce0ef224ba6b115ab454e8
DAltier <altierdana@gmail.com>
2025-10-21 20:37:45 -0700

    robofriends

---
+++ robofriends/vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? '/robofriends/' : '/',
+  server: {
+    host: '127.0.0.1', // Force IPv4
+    port: 5173,
+  },
+});
